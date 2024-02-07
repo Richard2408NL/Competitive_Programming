4911120a898b465ddeeea4a90ce6c92469f078ae
Ricardo Guerrero <A00835912@tec.mx>
2024-02-07 12:59:28 -0600

    Sesión 7 RAMcpp

---
+++ RAMcpp/Jan-Aug 24/Sesión 7/i.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+void solve(int n){
+    int r, avg; cin >> r >> avg;
+    pair<int, int> exam;
+
+    for(int i = 0 ; i < n ; i++)
+        cin >> exam.
+
+    
+
+}
+
+int32_t main()
+{
+    int n; cin >> n;
+
+    while(n--)
+        solve(n);
+    
+    
+    return 0;
+}
